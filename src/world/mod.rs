d4dabe02b8f6fcf04cd61b1614ea9b0bbd33c439
Sander Julien-Saint-Amand <sanderjsa@hotmail.com>
2020-12-26 23:30:10 +0100

    Move Entities to HashMap in World

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -6,27 +6,34 @@ pub use component::Component;
 pub use entity::Entity;
 pub use system::System;
 
+use std::any::TypeId;
+use std::collections::HashMap;
+
 /// World represents the global state of the game
 pub struct World {
-    entities: u64,
+    entities: HashMap<u64, Entity>,
+
+    components: HashMap<TypeId, Box<dyn Component>>,
+
+    next_free: u64,
 }
 
 impl World {
     /// Create a new World
     pub fn new() -> World {
-        World { entities: 0 }
+        World {
+            entities: HashMap::new(),
+            components: HashMap::new(),
+            next_free: 0,
+        }
     }
 
-    /// Get number of living entities
-    pub fn entities(&self) -> u64 {
+    /// Create a new entity and return its index
+    pub fn create_entity(&mut self) -> u64 {
         self.entities
-    }
-
-    /// Create a new entity and return it
-    pub fn add_entity(&mut self) -> Entity {
-        let entity = Entity::new(self.entities);
-        self.entities += 1;
-        entity
+            .insert(self.next_free, Entity::new(self.next_free));
+        self.next_free += 1;
+        self.next_free - 1
     }
 }
 
@@ -38,15 +45,28 @@ mod test {
     fn new_world() {
         let world = World::new();
 
-        assert!(world.entities() == 0);
+        assert!(world.entities.len() == 0);
+        assert!(world.components.len() == 0);
+        assert!(world.next_free == 0);
     }
 
     #[test]
-    fn get_index_one() {
+    fn create_one_entity() {
         let mut world = World::new();
 
-        let entity = world.add_entity();
+        let entity = world.create_entity();
 
-        assert!(entity.index() == 0);
+        assert!(entity == 0);
+    }
+
+    #[test]
+    fn create_two_entity() {
+        let mut world = World::new();
+
+        let entity1 = world.create_entity();
+        let entity2 = world.create_entity();
+
+        assert!(entity1 == 0);
+        assert!(entity2 == 1);
     }
 }
